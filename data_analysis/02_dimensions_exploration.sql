6ea303d2a61da6ba2e49430062c2e87855b21bec
aplotnyk <125077663+aplotnyk@users.noreply.github.com>
2025-10-09 21:21:23 +0200

    Create 02_dimensions_exploration.sql

---
+++ data_analysis/02_dimensions_exploration.sql
@@ -0,0 +1,7 @@
+-- Explore all countries our customers come from
+SELECT DISTINCT country FROM gold.dim_customers;
+
+-- Explore all major product categories
+SELECT DISTINCT category, subcategory, product_name
+FROM gold.dim_products
+ORDER BY 1,2,3;
