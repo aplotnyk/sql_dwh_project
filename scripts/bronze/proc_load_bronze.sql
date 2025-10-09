f2b245c5393f85dd0bce5d854f9a1895f3769bed
Anna Plotnykova <ann12cas@gmail.com>
2025-10-09 22:54:15 +0200

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -14,6 +14,8 @@ Parameters:
 
 Usage Example:
   EXEC bronze.load_bronze;
+
+!! The file path for each source file should be updated before running the script
 ============================================================================================================
 */
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
@@ -37,7 +39,8 @@ BEGIN
 
 		PRINT '>>> Inserting Data Into: bronze.crm_cust_info';
 		BULK INSERT bronze.crm_cust_info
-		FROM 'C:\Users\velod\OneDrive\Робочий стіл\Ann\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		-- Update the file path accordingly
+		FROM 'C:\Downloads\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -54,7 +57,8 @@ BEGIN
 
 		PRINT '>>> Inserting Data Into: bronze.crm_prd_info';
 		BULK INSERT bronze.crm_prd_info
-		FROM 'C:\Users\velod\OneDrive\Робочий стіл\Ann\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		-- Update the file path accordingly
+		FROM 'C:\Downloads\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -71,7 +75,8 @@ BEGIN
 
 		PRINT '>>> Inserting Data Into: bronze.crm_sales_details';
 		BULK INSERT bronze.crm_sales_details
-		FROM 'C:\Users\velod\OneDrive\Робочий стіл\Ann\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		-- Update the file path accordingly
+		FROM 'C:\Downloads\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -92,7 +97,8 @@ BEGIN
 
 		PRINT '>>> Inserting Data Into: bronze.erp_cust_az12';
 		BULK INSERT bronze.erp_cust_az12
-		FROM 'C:\Users\velod\OneDrive\Робочий стіл\Ann\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+		-- Update the file path accordingly
+		FROM 'C:\Downloads\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -109,7 +115,8 @@ BEGIN
 
 		PRINT '>>> Inserting Data Into: bronze.erp_loc_a101';
 		BULK INSERT bronze.erp_loc_a101
-		FROM 'C:\Users\velod\OneDrive\Робочий стіл\Ann\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+		-- Update the file path accordingly
+		FROM 'C:\Downloads\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -126,7 +133,8 @@ BEGIN
 
 		PRINT '>>> Inserting Data Into: bronze.erp_px_cat_g1v2';
 		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'C:\Users\velod\OneDrive\Робочий стіл\Ann\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+		-- Update the file path accordingly
+		FROM 'C:\Downloads\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
